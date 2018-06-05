8a9db3e4bbf4353aefd7a94fefe5603dfef69e9a
SolvarD <solvar@msn.com>
2018-06-05 15:24:37 +0200

    gulp minify

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+﻿var gulp = require('gulp');
+var minify = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var jsFiles = 'app/**/*.js',
+    jsDest = 'dist';
+gulp.task('start', function() {
+  return gulp.src(jsFiles)
+      .pipe(minify('app.min.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest(jsDest));
+});
